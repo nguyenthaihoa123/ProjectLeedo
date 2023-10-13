a126463dead87216dc2699917820c8e7c7e6e474
Nguyen Hao Nam <52100979@student.tdtu.edu.vn>
2023-10-13 17:08:12 +0700

    add 2 class interface

---
+++ ProjectN11/BanGiay_N11/BanGiay_N11/employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanGiay_N11
+{
+    internal class employee : person
+    {
+        private string cccd;
+        private string dept;
+        private string tkBank;
+        private string chucvu;
+        private double salary;
+        private string imgNV;
+
+        public employee(string id, string name, string birth, string sex, string sdt, string email, string pwd, string cccd, string dept, string tkBank, string chucvu, double salary, string imgNV) : base(id, name, birth, sex, sdt, email, pwd) { 
+            this.cccd = cccd;
+            this.dept = dept;
+            this.tkBank = tkBank;
+            this.chucvu = chucvu;
+            this.salary = salary;
+            this.imgNV = imgNV;
+        }
+        // phuong thuc get
+        public string getCCCD() {  return cccd; }
+        public string getDept() {  return dept; }
+        public string getBank() { return tkBank;}
+        public string getChucvu() {  return chucvu; }
+        public double getSalary() {  return salary; }
+        public string getImgNV() {  return imgNV;}
+        // phuong thuc set
+        public void setCCCD(string cccd) {  this.cccd = cccd; }
+        public void setDept(string dept) {  this.dept = dept; }
+        public void setTKBank(string tkBank) { this.tkBank = tkBank; }
+        public void setChucvu(string chucvu) { this.chucvu = chucvu; }
+        public void setSalary(double salary) { this.salary = salary; }
+        public void setImgNV(string imgNV) {  this.imgNV = imgNV; }
+    }
+}
---
+++ ProjectN11/BanGiay_N11/BanGiay_N11/person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanGiay_N11
+{
+    internal class person
+    {
+        protected string id;
+        protected string name;
+        protected string birth;
+        protected string sex;
+        protected string sdt;
+        protected string email;
+        protected string pwd;
+
+        public person(string id, string name, string birth, string sex, string sdt, string email, string pwd) {
+            this.id = id;
+            this.name = name;
+            this.birth = birth;
+            this.sex = sex;
+            this.sdt = sdt;
+            this.email = email;
+            this.pwd = pwd;
+        }
+        // phuong thuc get
+        public string getId() { return id; }
+        public string getName() { return name; }
+        public string getBirth() { return birth;}
+        public string getSex() {  return sex;}
+        public string getSdt() { return sdt;}
+        public string getEmail() { return email;}
+        public string getPwd() { return pwd;}
+        // phuong thuc set
+        public void setID(string id) { this.id = id;}
+        public void setName(string name) { this.name = name;}
+        public void setBirth(string birth) {  this.birth = birth;}
+        public void setSex(string sex) {  this.sex = sex;}
+        public void setSdt(string sdt) {  this.sdt = sdt;}
+        public void setEmail(string email) {  this.email = email;}
+        public void setPwd(string pwd) {  this.pwd = pwd;}
+    }
+}
