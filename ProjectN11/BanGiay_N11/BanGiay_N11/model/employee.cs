a110ef0f7e068106bb1791a4f4030525b1809227
Hoa <52100413@student.tdtu.edu.vn>
2023-10-25 00:08:10 +0700

    hoadeptrai toi 24

--- ProjectN11/BanGiay_N11/BanGiay_N11/model/employee.cs
+++ ProjectN11/BanGiay_N11/BanGiay_N11/model/employee.cs
@@ -6,37 +6,26 @@ using System.Threading.Tasks;
 
 namespace BanGiay_N11
 {
-    internal class employee: person
+    internal class employee : person
     {
-        private string cccd;
-        private string dept;
-        private string tkBank;
-        private string chucvu;
-        private double salary;
-        private string imgNV;
+        public string CCCD { get; set; }
+        public string Dept { get; set; }
+        public string TKBank { get; set; }
+        public string ChucVu { get; set; }
+        public double Salary { get; set; }
+        public string ImgNV { get; set; }
 
         public employee(string id, string name, string birth, string sex, string sdt, string email, string pwd, string cccd, string dept, string tkBank, string chucvu, double salary, string imgNV) : base(id, name, birth, sex, sdt, email, pwd)
         {
-            this.cccd = cccd;
-            this.dept = dept;
-            this.tkBank = tkBank;
-            this.chucvu = chucvu;
-            this.salary = salary;
-            this.imgNV = imgNV;
+            CCCD = cccd;
+            Dept = dept;
+            TKBank = tkBank;
+            ChucVu = chucvu;
+            Salary = salary;
+            ImgNV = imgNV;
         }
-        // phuong thuc get
-        public string getCCCD() { return cccd; }
-        public string getDept() { return dept; }
-        public string getBank() { return tkBank; }
-        public string getChucvu() { return chucvu; }
-        public double getSalary() { return salary; }
-        public string getImgNV() { return imgNV; }
-        // phuong thuc set
-        public void setCCCD(string cccd) { this.cccd = cccd; }
-        public void setDept(string dept) { this.dept = dept; }
-        public void setTKBank(string tkBank) { this.tkBank = tkBank; }
-        public void setChucvu(string chucvu) { this.chucvu = chucvu; }
-        public void setSalary(double salary) { this.salary = salary; }
-        public void setImgNV(string imgNV) { this.imgNV = imgNV; }
+        /*employee emp = new employee(...);
+        string cccdValue = emp.CCCD; // Lấy giá trị CCCD
+        emp.Salary = 50000; // Đặt giá trị Salary*/
     }
 }
--- ProjectN11/BanGiay_N11/BanGiay_N11/model/person.cs
+++ ProjectN11/BanGiay_N11/BanGiay_N11/model/person.cs
@@ -4,43 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BanGiay_N11
 {
     internal class person
     {
-        protected string id;
-        protected string name;
-        protected string birth;
-        protected string sex;
-        protected string sdt;
-        protected string email;
-        protected string pwd;
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Birth { get; set; }
+        public string Sex { get; set; }
+        public string Sdt { get; set; }
+        public string Email { get; set; }
+        public string Pwd { get; set; }
 
         public person(string id, string name, string birth, string sex, string sdt, string email, string pwd)
         {
-            this.id = id;
-            this.name = name;
-            this.birth = birth;
-            this.sex = sex;
-            this.sdt = sdt;
-            this.email = email;
-            this.pwd = pwd;
+            Id = id;
+            Name = name;
+            Birth = birth;
+            Sex = sex;
+            Sdt = sdt;
+            Email = email;
+            Pwd = pwd;
         }
-        // phuong thuc get
-        public string getId() { return id; }
-        public string getName() { return name; }
-        public string getBirth() { return birth; }
-        public string getSex() { return sex; }
-        public string getSdt() { return sdt; }
-        public string getEmail() { return email; }
-        public string getPwd() { return pwd; }
-        // phuong thuc set
-        public void setID(string id) { this.id = id; }
-        public void setName(string name) { this.name = name; }
-        public void setBirth(string birth) { this.birth = birth; }
-        public void setSex(string sex) { this.sex = sex; }
-        public void setSdt(string sdt) { this.sdt = sdt; }
-        public void setEmail(string email) { this.email = email; }
-        public void setPwd(string pwd) { this.pwd = pwd; }
     }
 }
