c7e71118626d37be0dbd088e7c18d9cd596e78d6
Nguyen Hao Nam <52100979@student.tdtu.edu.vn>
2023-10-26 17:37:58 +0700

    hnam 26/10

---
+++ ProjectN11/BanGiay_N11/BanGiay_N11/Database.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BanGiay_N11
+{
+    internal class Database
+    {
+        private string connectionString = @"Data Source=DESKTOP-S91JM4I\SQLEXPRESS2012;Initial Catalog=projectLeedo;Integrated Security=True";
+        private SqlConnection conn;
+        private string sql;
+        private DataTable dt;
+        private SqlCommand cmd;
+
+        public Database()
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connected failed:" + ex.Message);
+            }
+        }
+        //
+        public DataTable SelectData(string sql, List<CustomParameter> lstPara)
+        {
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                foreach (var para in lstPara)
+                {
+                    cmd.Parameters.AddWithValue(para.key, para.value);
+                }
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi load dữ liệu: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public DataRow Select(String sql)
+        {
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt.Rows[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi load thông tin chi tiết: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public int ExeCute(string sql, List<CustomParameter> lstPara)
+        {
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                //return (int)cmd.ExecuteScalar();
+                cmd.CommandType = CommandType.StoredProcedure;
+                foreach (var p in lstPara)
+                {
+                    cmd.Parameters.AddWithValue(p.key, p.value);
+                }
+                var rs = cmd.ExecuteNonQuery();
+                return (int)rs;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thực thi câu lệnh: " + ex.Message);
+                return -100;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ ProjectN11/BanGiay_N11/BanGiay_N11/model/CustomParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanGiay_N11
+{
+    internal class CustomParameter
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
